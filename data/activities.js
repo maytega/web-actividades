dbf3cb20f8ecb2a33d7530a448ff1f810eb329ca
maytega <maytega@uniovi.es>
2025-04-27 21:58:31 +0200

    ficheros de datos

---
+++ data/activities.js
@@ -0,0 +1,5 @@
+let activities = [{name:"UNICEF", money:0},
+                  {name:"Save the Children", money:0}
+]
+
+module.exports = activities
---
+++ data/users.js
@@ -0,0 +1,6 @@
+let users=[
+    {name:"user1", password:"user1", money:0},
+    {name:"user2", password:"user2", money:0}
+]
+
+module.exports = users
